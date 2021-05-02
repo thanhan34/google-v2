ce7e964ab89fc7d489e285766e9b68b2e75758f9
An Doan <dtan42@gmail.com>
2021-05-02 19:27:16 +1000

    modify env file

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ function Header() {
     }
     return (
         <header className="sticky top-0 bg-white">
-            <div className="flex w-full p-6 items-center">
+            <div className="flex items-center w-full p-6">
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
                     height={40}
@@ -25,16 +25,16 @@ function Header() {
                     className="cursor-pointer"
                     onClick={() => router.push("/")}
                 />
-                <form className="flex flex-grow border boder-gray-200 rounded-full flex-grow shadow-lg max-w-3xl items-center px-6 py-3 ml-10 mr-5">
+                <form className="flex items-center flex-grow max-w-3xl px-6 py-3 ml-10 mr-5 border rounded-full shadow-lg boder-gray-200">
                     <input
                         type="text"
                         ref={searchInputRef}
                         className="flex-grow w-full focus:outline-none"
                     />
-                    <XIcon className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125" onClick={() => (searchInputRef.current.value = "")} />
-                    <MicrophoneIcon className="h-6 mr-3 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
-                    <SearchIcon className="h-6 text-blue-500 hidden sm:inline-flex" />
-                    <button type="submit" onClick={search}> Search</button>
+                    <XIcon className="text-gray-500 transition duration-100 transform cursor-pointer h-7 sm:mr-3 hover:scale-125" onClick={() => (searchInputRef.current.value = "")} />
+                    <MicrophoneIcon className="hidden h-6 pl-4 mr-3 text-blue-500 border-l-2 border-gray-300 sm:inline-flex" />
+                    <SearchIcon className="hidden h-6 text-blue-500 sm:inline-flex" />
+                    <button type="submit" onClick={search} hidden> Search</button>
                 </form>
                 <Avatar url="https://scontent.fbne6-1.fna.fbcdn.net/v/t1.6435-9/172674233_10219188711817403_681016986696588621_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=dnb21LM3ViEAX9-PO38&_nc_ht=scontent.fbne6-1.fna&oh=8f61f5ef78896e8dec98274ec5369197&oe=60AF79AE"
                     className="ml-auto" />
